ff5f28ca84fb72d1e5e219e5d4a642921bfdfb24
Albasha99 <albashahmad.hu@gmail.com>
2022-11-28 04:21:47 +0100

    added gitignore for vcs

--- e-commerce/pages/index.tsx
+++ e-commerce/pages/index.tsx
@@ -1,6 +1,7 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
     return (
@@ -10,9 +11,7 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <h1>
-                Let's Build ShoPte
-            </h1>
+            <Header />
 
         </div>
     )
