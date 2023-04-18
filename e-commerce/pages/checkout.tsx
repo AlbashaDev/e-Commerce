72649aed41dd313a539e47dfa98ffe8e3ab752b7
Albasha99 <albashahmad.hu@gmail.com>
2023-04-18 09:00:19 +0200

    checkout page components "Refactored the user authentication logic to improve security and performance. Added password hashing and salting to prevent password leaks. Reorganized code structure to reduce redundancy and improve readability. Updated database queries to use prepared statements for added security."

--- e-commerce/pages/checkout.tsx
+++ e-commerce/pages/checkout.tsx
@@ -5,12 +5,12 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Currency from "react-currency-formatter";
 import { useSelector } from "react-redux";
-// import Stripe from "stripe";
+import Stripe from "stripe";
 import Button from "../components/Button";
 import CheckoutProduct from "../components/CheckoutProduct";
 import { selectBasketItems, selectBasketTotal } from "../redux/basketSlice";
-// import { fetchPostJSON } from "../utils/api-helpers";
-// import getStripe from "../utils/get-stripejs";
+import { fetchPostJSON } from "../utils/api-helpers";
+import getStripe from "../utils/get-stripejs";
 
 function Checkout() {
   const items = useSelector(selectBasketItems);
@@ -19,7 +19,7 @@ function Checkout() {
   const [groupedItemsInBasket, setGroupedItemsInBasket] = useState(
     {} as { [key: string]: Product[] }
   );
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const groupedItems = items.reduce((results, item) => {
       (results[item._id] = results[item._id] || []).push(item);
@@ -28,6 +28,39 @@ function Checkout() {
 
     setGroupedItemsInBasket(groupedItems);
   }, [items]);
+
+  const createCheckoutSession = async () => {
+    setLoading(true);
+
+    const checkoutSession: Stripe.Checkout.Session = await fetchPostJSON(
+      "/api/checkout_sessions",
+      {
+        items: items,
+      }
+    );
+
+    // Internal Server Error
+    if ((checkoutSession as any).statusCode === 500) {
+      console.error((checkoutSession as any).message);
+      return;
+    }
+
+    // Redirect to checkout
+    const stripe = await getStripe();
+    const { error } = await stripe!.redirectToCheckout({
+      // Make the id field from the Checkout Session creation API response
+      // available to this file, so you can provide it as parameter here
+      // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+      sessionId: checkoutSession.id,
+    });
+
+    // If `redirectToCheckout` fails due to a browser or network
+    // error, display the localized error message to your customer
+    // using `error.message`.
+    console.warn(error.message);
+
+    setLoading(false);
+  };
 
   return (
     <div className="min-h-screen overflow-hidden bg-[#E7ECEE]">
@@ -85,7 +118,7 @@ function Checkout() {
                 <div className="flex justify-between pt-4 text-xl font-semibold">
                   <h4>Total</h4>
                   <h4>
-                    <Currency quantity={basketTotal} currency="HUF" />
+                    <Currency quantity={basketTotal} currency="KWD" />
                   </h4>
                 </div>
               </div>
@@ -114,16 +147,16 @@ function Checkout() {
                     <h4 className="mb-4 flex flex-col text-xl font-semibold">
                       Pay in full
                       <span>
-                        <Currency quantity={basketTotal} currency="HUF" />
+                        <Currency quantity={basketTotal} currency="KWD" />
                       </span>
                     </h4>
 
                     <Button
                       noIcon
-                      // loading={loading}
+                      loading={loading}
                       title="Check Out"
                       width="w-full"
-                      // onClick={createCheckoutSession}
+                      onClick={createCheckoutSession}
                     />
                   </div>
                 </div>
@@ -135,5 +168,4 @@ function Checkout() {
     </div>
   );
 }
-
 export default Checkout;
